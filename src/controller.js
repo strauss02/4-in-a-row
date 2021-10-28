6bef22761156ca941c792ab6dab42cd2b7026a5c
Ido Strauss <strauss02@hotmail.co.il>
2021-10-28 21:05:40 +0300

    init folder

---
+++ src/controller.js
@@ -0,0 +1,11 @@
+import FourInARow from './model'
+import View from './view'
+
+class Controller {
+  constructor() {
+    this.model = new FourInARow()
+    this.view = new View()
+
+    //bind data from dom (view) to actions from model
+  }
+}
